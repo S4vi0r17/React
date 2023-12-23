757a1dad0efd7d42d410d2c3cffe23ce9ff1d023
S4v!0r <gbp17@outlook.com>
2023-12-22 21:56:25 -0500

    Refactor code for improved performance

---
+++ click-counter/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import logo from './img/logo.png';
+
+function App() {
+  return (
+    <div className='App'>
+      <div className='logo-container'>
+        <img className='logo' src={logo} alt='logo' />
+      </div>
+      <div className='main-container'>
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;

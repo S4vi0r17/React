af1814cd1a19aeb8f6f38f56a39010ba07135068
S4v!0r <gbp17@outlook.com>
2024-08-08 17:31:13 -0500

    Add: App.jsx

---
+++ Dev/04-CounterApp/src/App.jsx
@@ -0,0 +1,23 @@
+import PropTypes from 'prop-types';
+
+const App = ({ title, subtitle, name }) => (
+    <div>
+        <h1>{title}</h1>
+        <h2>{subtitle}</h2>
+        <p>Hello, {name}!</p>
+    </div>
+);
+
+export default App;
+
+App.propTypes = {
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    name: PropTypes.string,
+};
+
+App.defaultProps = {
+    title: 'Hello World',
+    subtitle: 'Welcome to the world of React',
+    name: 'Stranger',
+};
--- Dev/04-CounterApp/src/main.jsx
+++ Dev/04-CounterApp/src/main.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// import App from './App.jsx'
-import CounterApp from './CounterApp.jsx'
+import App from './App.jsx'
+// import CounterApp from './CounterApp.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <CounterApp value={20} />
+    {/* <CounterApp value={20} /> */}
+    <App />
   </React.StrictMode>,
 )

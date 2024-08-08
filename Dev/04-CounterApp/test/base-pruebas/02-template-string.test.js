eb91687109ddc62e5be0aa97abd42382efa12329
S4v!0r <gbp17@outlook.com>
2024-08-07 22:36:01 -0500

    refactor: Update test files to use Jest globals for describe, test, and expect

--- Dev/04-CounterApp/test/base-pruebas/02-template-string.test.js
+++ Dev/04-CounterApp/test/base-pruebas/02-template-string.test.js
@@ -1,6 +1,7 @@
+import { describe, test, expect } from '@jest/globals';
 import { getSaludo } from '../../src/base-pruebas/02-template-string';
 
-describe('Pruebas en el archivo 02-template-string.test.js', () => {
+describe('Pruebas en el archivo 02-template-string.js', () => {
 
     test('getSaludo debe retornar Hola Eder', () => {
 
--- Dev/04-CounterApp/test/demo.test.js
+++ Dev/04-CounterApp/test/demo.test.js
@@ -1,3 +1,5 @@
+import { describe, test, expect } from '@jest/globals';
+
 describe('Pruebas en <DemoComponent />', () => {
 
     test('Esta prueba no puede fallar', () => {

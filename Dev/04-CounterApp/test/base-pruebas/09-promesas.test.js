6e4019e85b71cc87e5d26c85dc6f2c6048f9a036
S4v!0r <gbp17@outlook.com>
2024-08-07 23:44:15 -0500

    refactor: Update import statements and remove unused code in 09-promesas.js and 08-imp-exp.js files

---
+++ Dev/04-CounterApp/test/base-pruebas/09-promesas.test.js
@@ -0,0 +1,35 @@
+import { describe, expect, test } from '@jest/globals';
+import { getHeroeByIdAsync } from '../../src/base-pruebas/09-promesas';
+import heroes from '../../src/data/heroes';
+
+describe('Pruebas con promesas', () => {
+
+    test('getHeroeByIdAsync debe retornar un héroe async', (done) => {
+
+        const id = 1;
+
+        getHeroeByIdAsync(id)
+            .then(heroe => {
+
+                expect(heroe).toBe(heroes[0]);
+                done();
+
+            });
+
+    });
+
+    test('debe obtener un error si el héroe por id no existe', (done) => {
+
+        const id = 10;
+
+        getHeroeByIdAsync(id)
+            .catch(error => {
+
+                expect(error).toBe('No se pudo encontrar el héroe');
+                done();
+
+            });
+
+    });
+
+});

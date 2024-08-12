9c66392510b02b9625b92a5f2591317a3ff7a1fe
S4v!0r <gbp17@outlook.com>
2024-08-12 11:41:15 -0500

    refactor: Remove unused FocusScreen component and add Layout component with useLayoutEffect hook

---
+++ Dev/05-hooks/src/05-useLayoutEffect/Layout.jsx
@@ -0,0 +1,41 @@
+import { useCounter } from "../hooks/useCounter"
+import { useFetch } from "../hooks/useFetch"
+import LoadingMessage from "../03-examples/LoadingMessage"
+import PokemonCard from "../03-examples/PokemonCard"
+
+const Layout = () => {
+    const { counter, increase, decrease } = useCounter(1)
+    const { data, isLoading, hasError } = useFetch(`https://pokeapi.co/api/v2/pokemon/${counter}`)
+
+    return (
+        <div className="container text-center mt-5">
+            <h1 className="mb-4">
+                Información del Pokémon
+            </h1>
+
+            <hr className="mb-4" />
+
+            {isLoading ? <LoadingMessage /> : <PokemonCard {...data} />}
+
+            <div className="d-flex justify-content-center mt-4">
+                <button
+                    className="btn btn-outline-primary me-3"
+                    onClick={counter > 1 ? decrease : () => { }}
+                    disabled={counter <= 1}
+                >
+                    Anterior Pokémon
+                </button>
+                <button
+                    className="btn btn-outline-primary"
+                    onClick={increase}
+                >
+                    Siguiente Pokémon
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default Layout
+
+/* Not implemented in this example */
--- Dev/05-hooks/src/main.jsx
+++ Dev/05-hooks/src/main.jsx
@@ -6,7 +6,8 @@ import { createRoot } from 'react-dom/client'
 // import SimpleForm from './02-useEffect/SimpleForm'
 // import FormWithCustomHook from './02-useEffect/FormWithCustomHook'
 // import MultipleCustomHooks from './03-examples/MultipleCustomHooks'
-import FocusScreen from './04-useRef/FocusScreen'
+// import FocusScreen from './04-useRef/FocusScreen'
+import Layout from './05-useLayoutEffect/Layout'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -16,6 +17,7 @@ createRoot(document.getElementById('root')).render(
     {/* <SimpleForm /> */}
     {/* <FormWithCustomHook /> */}
     {/* <MultipleCustomHooks /> */}
-    <FocusScreen />
+    {/* <FocusScreen /> */}
+    <Layout />
   </StrictMode>,
 )
